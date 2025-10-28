6dd81691deb62dc0f947627be42c4f141a219e1b
gabriellac2002 <gabriellacsilva2002@gmail.com>
2025-10-28 10:29:21 -0300

    feat: Refactor layout structure by introducing PageLayout component and updating product, user, shopping, and upload pages

---
+++ src/app/(admin)/shopping/page.tsx
@@ -0,0 +1,11 @@
+import { Title } from "@mantine/core";
+
+export const ShoppingPage: React.FC = () => {
+  return (
+    <>
+      <Title order={1}>Compras</Title>
+    </>
+  );
+};
+
+export default ShoppingPage;
